11d0be8c2a54b6d67a259ce6d6a9514e10c716c6
shathi <shathi@gmail.com>
2023-11-21 09:49:14 +0600

    add meu item

--- index.js
+++ index.js
@@ -37,71 +37,69 @@ async function run() {
 
         // middlwarae
 
-        const verifyToken = (req, res, next) =>{
-            if(!req.headers.authorization){
-                return res.status(401).send({message: "Forbidden Access"})
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "Forbidden Access" })
             }
             const token = req.headers.authorization.split(' ')[1]
-            if(!token){
-                return res.status(401).send({message: "Forbidden Access"})
+            if (!token) {
+                return res.status(401).send({ message: "Forbidden Access" })
             }
-            jwt.verify(token, process.env.SecretKey, (err, decoded) =>{
-                if(err){
-                    return res.status(401).send({message: "Forbidden Access"})
+            jwt.verify(token, process.env.SecretKey, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "Forbidden Access" })
                 }
                 req.decoded = decoded
-                console.log('Hello I am Done')
                 next()
             })
         }
 
-        const verifyAdmin = async(req, res, next) =>{
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email
-            const query = {email: email}
+            const query = { email: email }
             const user = await userCollection.findOne(query)
             const isAdmin = user?.role === 'admin'
-            if(!isAdmin){
-                return res.status(403).send({message: 'Forbidden Access'})
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'Forbidden Access' })
             }
             next()
         }
 
 
         // jwt releted API
-        app.post('/jwt',(req, res) =>{
+        app.post('/jwt', (req, res) => {
             const user = req.body
-            const token = jwt.sign(user, process.env.SecretKey, {expiresIn:'1h'})
-            res.send({token})
+            const token = jwt.sign(user, process.env.SecretKey, { expiresIn: '1h' })
+            res.send({ token })
         })
 
         // usr section
 
-        app.get('/user', verifyToken, verifyAdmin, async(req, res) =>{
+        app.get('/user', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
 
-        app.get('/user/admin/:email', verifyToken, async(req, res) =>{
+        app.get('/user/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email
-            console.log(email)
-            if(email !== req.decoded.email){
-                return res.status(403).send({message:'Unauthorize Access'})
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Unauthorize Access' })
             }
-            const query = {email: email}
+            const query = { email: email }
             const user = await userCollection.findOne(query)
             let admin = false
-            if(user){
+            if (user) {
                 admin = user?.role === 'admin'
             }
             res.send(admin)
         })
 
-        app.patch('/user/:id', async(req, res) =>{
+        app.patch('/user/:id', async (req, res) => {
             const Id = req.params.id
-            const query = {_id: new ObjectId(Id)}
+            const query = { _id: new ObjectId(Id) }
             const Admin = {
                 $set: {
-                    role:'admin'
+                    role: 'admin'
                 }
             }
             const result = await userCollection.updateOne(query, Admin)
@@ -109,51 +107,65 @@ async function run() {
 
         })
 
-        app.delete('/user/:id', async(req, res) =>{
+        app.delete('/user/:id', async (req, res) => {
             const Id = req.params.id
-            const query = {_id: new ObjectId(Id)}
+            const query = { _id: new ObjectId(Id) }
             const result = await userCollection.deleteOne(query)
+            console.log(result)
             res.send(result)
         })
 
-        app.post('/user', async(req, res) =>{
+        app.post('/user', async (req, res) => {
             const user = req.body
-            const query = {email: user.email}
+            const query = { email: user.email }
             const CheckExiting = await userCollection.findOne(query)
-            if(CheckExiting){
-                return {message:'User Already Exist', insertedId: null}
+            if (CheckExiting) {
+                return { message: 'User Already Exist', insertedId: null }
             }
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
 
-        app.get('/menu', async(req, res) =>{
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
 
-        app.get('/review', async(req, res) =>{
+        app.delete('/menu/:id',verifyToken, verifyAdmin, async (req, res) => {
+            const Id = req.params.id
+            const query = {_id: new ObjectId(Id) }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const menuItem = req.body
+            const result = await menuCollection.insertOne(menuItem)
+            res.send(result)
+        })
+
+        app.get('/review', async (req, res) => {
             const result = await ReviewCollection.find().toArray()
             res.send(result)
         })
 
         // product card section
 
-        app.get('/cards', async(req, res) =>{
+        app.get('/cards', async (req, res) => {
             const email = req.query.email
-            const query = {user: email}
+            const query = { user: email }
             const result = await CardCollection.find(query).toArray()
             res.send(result)
         })
-        app.delete('/cards/:id', async(req, res) =>{
+        app.delete('/cards/:id', async (req, res) => {
             const Id = req.params.id
-            const query = {_id: new ObjectId(Id)}
+            const query = { _id: new ObjectId(Id) }
             const result = await CardCollection.deleteOne(query)
             res.send(result)
         })
 
-        app.post('/cards', async(req, res) =>{
+        app.post('/cards', async (req, res) => {
             const Item = req.body
             const result = await CardCollection.insertOne(Item)
             res.send(result)
@@ -165,7 +177,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-    
+
     }
 }
 run().catch(console.dir);
